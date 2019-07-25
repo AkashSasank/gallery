d7be867a88eba80b6fa119bf321d922326069e06
akashc <akashc@qburst.com>
2019-07-25 12:19:50 +0530

    First commit

---
+++ JS/javascript1.js
@@ -0,0 +1,38 @@
+function load() {
+    for(var j =0;j< data.length;j++){
+        var i = document.getElementById(data[j].img_id);
+        i.setAttribute('src',data[j].url);
+        var k = document.getElementById(data[j].name_id);
+        k.innerHTML = data[j].name;
+    }
+  
+}
+ 
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
---
+++ JSON/urls.js
@@ -0,0 +1,6 @@
+var data = [{"img_id" : "i1", "url" : "./images/chimp1.jpg","name_id":"d1","name":"Primates"},
+            {"img_id" : "i2", "url" : "./images/snake1.jpg","name_id":"d2","name":"Snakes"},
+            {"img_id" : "i3", "url" : "./images/cat1.jpg","name_id":"d3","name":"Big Cats"},
+            {"img_id" : "i4", "url" : "./images/bird1.jpg","name_id":"d4","name":"Birds"},
+            {"img_id" : "i5", "url" : "./images/ele.jpg","name_id":"d5","name":"Elephants"},
+            {"img_id" : "i6", "url" : "./images/zebra.jpg","name_id":"d6","name":"Grass land beasts"}];
